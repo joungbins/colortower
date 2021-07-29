2a1a2b0aa44749aa1336994d06ea0d682b7c9cf6
joungbins <86279518+joungbins@users.noreply.github.com>
2021-07-29 19:47:45 +0900

    1.0

---
+++ media/text/strings.js
@@ -0,0 +1,54 @@
+var _STRINGS = {
+	"Ad": {
+		"Mobile": {
+			"Preroll": {
+				"ReadyIn": "The game is ready in ",
+				"Loading": "Your game is loading...",
+				"Close": "Close",
+			},
+			"Header": {
+				"ReadyIn": "The game is ready in ",
+				"Loading": "Your game is loading...",
+				"Close": "Close",
+			},
+			"End": {
+				"ReadyIn": "Advertisement ends in ",
+				"Loading": "Please wait ...",
+				"Close": "Close",
+			},
+		},
+	},
+
+	"Splash": {
+		"Loading": "Loading ...",
+		"LogoLine1": "Some text here",
+		"LogoLine2": "powered by MarketJS",
+		"LogoLine3": "none",
+		"TapToStart": "TAP TO START",
+	},
+
+	"MainMenu": {
+		"Play": "PLAY",
+		"SelectLevel": "SELECT LEVEL",
+	},
+	"Game": {
+		"Settings": "Settings",
+		"JumpToLevel": "Jump to Level",
+		"SelectLevel": "Select Level",
+		"Active": "Active",
+		"Ready": "Ready",
+		"GameOver": "Game Over",
+		"LevelClear": "Level Clear",
+		"Score": "SCORE",
+		"Highscore": "HIGHSCORE",
+		"NewHighscore": "NEW HIGHSCORE",
+
+
+		"DailyRewards": "Daily Rewards",
+		"DailyRewardBottomText": "Return daily to earn rewards!",
+		"Day": "Day ",
+		"Collect": "Collect",
+		"RewardCollected": "Reward Collected",
+		"Continue": "Continue",
+	}
+};
